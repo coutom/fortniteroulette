0875aff6d9f427a570b9ec9b8725f6599b3aef6d
Tom <tommycouto123456789@gmail.com>
2019-05-12 15:43:32 -0400

    version 1.0

--- app/src/main/java/com/example/fortniteroulette/FirstRoll.java
+++ app/src/main/java/com/example/fortniteroulette/FirstRoll.java
@@ -1,14 +1,30 @@
 package com.example.fortniteroulette;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FirstRoll extends AppCompatActivity {
+
+    Button roll;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.first_roll);
+
+        roll = (Button) findViewById(R.id.roll_btn);
+
+        roll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FirstRoll.this, Roll.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 
 }
