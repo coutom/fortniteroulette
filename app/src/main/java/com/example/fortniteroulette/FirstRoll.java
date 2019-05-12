dd21f6016449cfe30d61b5317e1e6e592e32f4c0
Tom <tommycouto123456789@gmail.com>
2019-05-11 21:20:35 -0400

    Initial Commit

---
+++ app/src/main/java/com/example/fortniteroulette/FirstRoll.java
@@ -0,0 +1,14 @@
+package com.example.fortniteroulette;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FirstRoll extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_roll);
+    }
+
+}
---
+++ app/src/main/java/com/example/fortniteroulette/Roll.java
@@ -0,0 +1,13 @@
+package com.example.fortniteroulette;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Roll extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_roll);
+    }
+}
