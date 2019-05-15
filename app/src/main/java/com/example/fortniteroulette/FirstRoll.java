81a6ac4606a52a812c781528b8f1e970c55769a3
jack <jackjm22@outlook.com>
2019-05-15 18:31:16 -0400

    Add animated text

--- app/src/main/java/com/example/fortniteroulette/FirstRoll.java
+++ app/src/main/java/com/example/fortniteroulette/FirstRoll.java
@@ -6,14 +6,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
 public class FirstRoll extends AppCompatActivity {
 
     ImageButton roll;
+    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.first_roll);
+
+        MobileAds.initialize(this, "ca-app-pub-6663849184001520~6055836060");
+
+        mAdView = findViewById(R.id.adView1);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
 
         roll = (ImageButton) findViewById(R.id.roll_btn);
         roll.setBackgroundResource(0);
--- app/src/main/java/com/example/fortniteroulette/Roll.java
+++ app/src/main/java/com/example/fortniteroulette/Roll.java
@@ -6,6 +6,11 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.hanks.htextview.evaporate.EvaporateTextView;
+
 import java.util.Random;
 
 public class Roll extends AppCompatActivity {
@@ -19,12 +24,12 @@ public class Roll extends AppCompatActivity {
     private String dNum;
     private String dLet;
 
-    private TextView weaponText;
-    private TextView healText;
-    private TextView dropText;
-    private TextView trapText;
-    private TextView buildText;
-    private TextView specText;
+    private EvaporateTextView weaponText;
+    private EvaporateTextView healText;
+    private EvaporateTextView dropText;
+    private EvaporateTextView trapText;
+    private EvaporateTextView buildText;
+    private EvaporateTextView specText;
 
     ImageButton roll2;
 
@@ -43,17 +48,25 @@ public class Roll extends AppCompatActivity {
             "Trained Soldier! No aiming down sights", "Four Corners! All team members must land in different corners", "Lead Feet! No sprinting", "Trickshot Master! Must 360 before shooting",
             "Shy Guy! No entering named areas", "Disrespect! Must dance before shooting", "Lightweight! Only 3 inventory spots can be used"};
 
+    private AdView mAdView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_roll);
 
-        weaponText = (TextView) findViewById(R.id.weapons_txt2);
-        healText = (TextView) findViewById(R.id.healing_txt2);
-        dropText = (TextView) findViewById(R.id.dropzone_txt2);
-        trapText = (TextView) findViewById(R.id.traps_txt2);
-        buildText = (TextView) findViewById(R.id.building_txt2);
-        specText = (TextView) findViewById(R.id.special_txt2);
+        MobileAds.initialize(this, "ca-app-pub-6663849184001520~6055836060");
+
+        mAdView = findViewById(R.id.adView2);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+        weaponText = (EvaporateTextView) findViewById(R.id.weapons_txt2);
+        healText = (EvaporateTextView) findViewById(R.id.healing_txt2);
+        dropText = (EvaporateTextView) findViewById(R.id.dropzone_txt2);
+        trapText = (EvaporateTextView) findViewById(R.id.traps_txt2);
+        buildText = (EvaporateTextView) findViewById(R.id.building_txt2);
+        specText = (EvaporateTextView) findViewById(R.id.special_txt2);
         roll2 = (ImageButton) findViewById(R.id.roll_btn2);
 
         roll2.setBackgroundResource(0);
@@ -84,41 +97,41 @@ public class Roll extends AppCompatActivity {
             //pick random weapon text and show it
             Random random = new Random();
             String weapon = weaponList[random.nextInt(weaponList.length)];
-            weaponText.setText("Weapons: " + weapon);
+            weaponText.animateText("Weapons: " + weapon);
         }
 
         if(healBoo) {
             //pick random healing text and show it
             Random random = new Random();
             String heal = healingList[random.nextInt(healingList.length)];
-            healText.setText("Healing: " + heal);
+            healText.animateText("Healing: " + heal);
         }
 
         if(dropBoo) {
             //pick random drop zone text and show it
             String drop = getDropzone();
-            dropText.setText("Healing: " + drop);
+            dropText.animateText("Healing: " + drop);
         }
 
         if(trapBoo) {
             //pick a random trap text and show it
             Random random = new Random();
             String trap = trapList[random.nextInt(trapList.length)];
-            trapText.setText("Traps: " + trap);
+            trapText.animateText("Traps: " + trap);
         }
 
         if(buildBoo) {
             //pick a random building text and show it
             Random random = new Random();
             String build = buildingList[random.nextInt(buildingList.length)];
-            buildText.setText("Building: " + build);
+            buildText.animateText("Building: " + build);
         }
 
         if(specBoo) {
             //pick a random special text and show it
             Random random = new Random();
             String spec = specialList[random.nextInt(specialList.length)];
-            specText.setText("Special: " + spec);
+            specText.animateText("Special: " + spec);
         }
     }
 
